7181f04070f0bb93b19c214312308bd6853349a6
mbelen <beel.betanzoo@hotmail.com.ar>
2024-06-18 01:11:31 -0300

    correccion

--- back/src/models/Movie.js
+++ back/src/models/Movie.js
@@ -3,40 +3,41 @@ const mongoose = require("mongoose");
 const movieSchema = new mongoose.Schema({
     title: {
         type: String,
-       // required: true, 
+       required: true, 
     },
     year: {
         type: Number,
-        //required: true,
-       // min: 1888,
-       // max: 2024,
+        required: true,
+       min: 1888,
+       max: 2024,
     },
     director: {
         type: String,
-        //required: true,
+        required: true,
     },
     duration: {
         type: Number,
-       // min: 50,
-       // max: 400,
-       // required: true,
+       min: 50,
+        max: 400,
+        required: true,
     },
     genre: {
         type: Array,
-       // required: true,
+        required: true,
     },
     rate: {
         type: Number,
-       // min: 1,
-       // max: 10,
-       // required: true,
+        min: 1,
+        max: 10,
+        required: true,
     },
     poster: {
         type: String,
-       // required: true,
+       required: true,
     }
 });
 
 const Movies = mongoose.model("Movies", movieSchema, "Movies");//este es el traductor, la conexion especifica
-//EL TERCER PARAMETRO ES PARA ESPECIFICARLE explícitamente el nombre de la colección en MongoDB. 
+//EL TERCER PARAMETRO ES PARA ESPECIFICARLE SIN VUELTAS A QUE 
+module.exports = { Movies }; 
 
